d93fd29e74b21b6691433be66dda9bf954ddb91d
Bommuraja <rbommuraja@outlook.com>
2018-08-31 07:29:14 +0530

    Primary API creation

---
+++ PhotosAPI/Controllers/PhotosController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+using System.Web.Http.Description;
+using PhotosAPI;
+
+namespace PhotosAPI.Controllers
+{
+    public class PhotosController : ApiController
+    {
+        private CollectionEntities db = new CollectionEntities();
+
+        // GET: api/Photos
+        public IQueryable<Photo> GetPhotos()
+        {
+            return db.Photos;
+        }
+
+        // GET: api/Photos/5
+        [ResponseType(typeof(Photo))]
+        public async Task<IHttpActionResult> GetPhoto(long id)
+        {
+            Photo photo = await db.Photos.FindAsync(id);
+            if (photo == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(photo);
+        }
+
+        // PUT: api/Photos/5
+        [ResponseType(typeof(void))]
+        public async Task<IHttpActionResult> PutPhoto(long id, Photo photo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != photo.PhotoID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(photo).State = EntityState.Modified;
+
+            try
+            {
+                await db.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!PhotoExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Photos
+        [ResponseType(typeof(Photo))]
+        public async Task<IHttpActionResult> PostPhoto(Photo photo)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Photos.Add(photo);
+            await db.SaveChangesAsync();
+
+            return CreatedAtRoute("DefaultApi", new { id = photo.PhotoID }, photo);
+        }
+
+        // DELETE: api/Photos/5
+        [ResponseType(typeof(Photo))]
+        public async Task<IHttpActionResult> DeletePhoto(long id)
+        {
+            Photo photo = await db.Photos.FindAsync(id);
+            if (photo == null)
+            {
+                return NotFound();
+            }
+
+            db.Photos.Remove(photo);
+            await db.SaveChangesAsync();
+
+            return Ok(photo);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool PhotoExists(long id)
+        {
+            return db.Photos.Count(e => e.PhotoID == id) > 0;
+        }
+    }
+}
